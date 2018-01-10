c3bc7de45828434aade015c03e003d8412c70b79
Aleksander Maj <aleksander.maj@htdevelopers.com>
2018-01-10 16:42:36 +0100

    Embed AVPlayerViewController in a container view to show its non-fullscreen capabilities.

--- S3Video/AppDelegate.swift
+++ S3Video/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return AWSMobileClient.sharedInstance().interceptApplication(application, didFinishLaunchingWithOptions: launchOptions)
+        return true//AWSMobileClient.sharedInstance().interceptApplication(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- S3Video/ViewController.swift
+++ S3Video/ViewController.swift
@@ -11,39 +11,38 @@ import AVFoundation
 import AVKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var playButton: UIButton!
 
-    lazy var videoItem: AVPlayerItem = {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    let playerVC = AVPlayerViewController()
+
+    var videoItem: AVPlayerItem {
         let videoItem = AVPlayerItem(asset: videoAsset)
         return videoItem
-    }()
+    }
 
-    lazy var videoAsset: AVURLAsset = {
+    var videoAsset: AVURLAsset {
         let url = URL(string: "https://s3-eu-west-1.amazonaws.com/aleksandermajhtd/IMG_0987.m4v")!
-
-        //let url = URL(string: "https://s3-eu-west-1.amazonaws.com/svideo-hosting-mobilehub-588546466/example-video-1.mp4")!
         let asset = AVURLAsset(url: url)
         return asset
-    }()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let player = AVPlayer(playerItem: videoItem)
+        playerVC.player = player
+
+        addChildViewController(playerVC)
+        self.containerView.addSubviewAndConstraintToEdges(playerVC.view)
+        playerVC.didMove(toParentViewController: self)
+
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
     @IBAction func playButtonTapped() {
-        let player = AVPlayer(playerItem: videoItem)
-        let playerVC = AVPlayerViewController()
-        playerVC.player = player
-        present(playerVC, animated: true) {
-            playerVC.player?.play()
-        }
+        playerVC.player?.play()
     }
 
 }
