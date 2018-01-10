915972a708cefe72d3cf530259d427d861166433
Aleksander Maj <aleksander.maj@htdevelopers.com>
2018-01-10 12:28:31 +0100

    Set up the project. Init and install pods.

---
+++ S3Video/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S3Video
+//
+//  Created by Aleksander Maj on 10/01/2018.
+//  Copyright © 2018 Aleksander Maj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
